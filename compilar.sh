a45ebb0e81af8df8923d1f587108c5b27a1f3fdd
VicdeJuan <dejuan.victor@gmail.com>
2016-06-24 11:10:25 +0200

    Acknowledgements,glossary, references fixed and burocracy needed

--- compilar.sh
+++ compilar.sh
@@ -10,6 +10,7 @@ makeglossaries main
 pdflatex main.tex
 pdflatex main.tex
 #mv main.pdf "Trabajo de Fin de Grado.pdf"
+mv main.pdf "TFG1516-GII-VíctorDeJuanSanz.pdf"
 
 # Limpiar archivos auxiliares
 #./limpiar.sh
